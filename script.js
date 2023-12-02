8c3f96f582b0f29cd8408e14124b24f89102316e
akshay <akshaynellikatte2002@gmail.com>
2023-12-03 00:15:34 +0530

    added content

--- script.js
+++ script.js
@@ -29,3 +29,37 @@ window.onscroll = () => {
     navbar.classList.remove('active');
 }
 
+    function scrollToAbout() {
+        const aboutSection = document.getElementById('about');
+        aboutSection.scrollIntoView({ behavior: 'smooth' });
+    }
+    function submitForm() {
+        const form = document.getElementById("contactForm");
+        const formData = new FormData(form);
+    
+        fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            body: JSON.stringify(Object.fromEntries(formData)),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8",
+            },
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                
+                document.getElementById("submitMessage").innerHTML =
+                    "Form submitted successfully!";
+                document.getElementById("submitMessage").style.color = "green";
+                form.reset();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+                
+                document.getElementById("submitMessage").innerHTML =
+                    "Error submitting the form. Please try again.";
+                document.getElementById("submitMessage").style.color = "red";
+            });
+    }
+    
+
+
